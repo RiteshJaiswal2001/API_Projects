da71f8066018b2aa0f8f35ee314d6c14c90ecba8
Ritesh kumar <rkjais10022001@gmail.com>
2026-03-30 18:04:46 +0530

    weather API Project is completed with its all requirements.

--- Weather_API/index.js
+++ Weather_API/index.js
@@ -1,54 +1,25 @@
 import axios from "axios";
 import dotenv from "dotenv";
 import client from "./db.js";
+import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 dotenv.config();
 
-const base_url = "http://api.weatherapi.com/v1";
-const API_method = "current.json";
-
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const base_url = "http://api.weatherapi.com/v1/current.json";
 
-const api = `${base_url}/${API_method}`;
-
-const place = "Patna";
-
-async function weatherResponseFetch() {
-  console.log(`${api}?key=${WEATHER_API_KEY}&q=${place}`);
-
-  //   try {
-  //     const response = await fetch(`${api}?key=${WEATHER_API_KEY}&q=${place}`);
-  //     const data = await response.json();
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(`Error:${error.message}`);
-
-  //   }
-  const params = new URLSearchParams({
-    key: WEATHER_API_KEY,
-    q: place,
-  });
+async function fetchWeatherResponse(city) {
   try {
-    const response = await fetch(`${api}?${params}`);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(`Error:${error.message}`);
-  }
-}
-
-// weatherResponseFetch();
-
-async function weatherResponseAxios() {
-  try {
-    const response = await axios(`${api}`, {
+    const response = await axios(base_url, {
       params: {
         key: WEATHER_API_KEY,
-        q: place,
+        q: city,
       },
     });
-
-    // console.log(response.data);
 
     return response.data;
   } catch (error) {
@@ -56,8 +27,8 @@ async function weatherResponseAxios() {
   }
 }
 
-async function getWeatherRedisDetils() {
-  const res = await client.hGetAll(`${place}`);
+async function getWeatherDetils(city) {
+  const res = await client.hGetAll(city);
 
   // console.log(res);
 
@@ -72,15 +43,54 @@ async function getWeatherRedisDetils() {
 
   console.log("Response is not available in database.");
 
-  const WeatherData = await weatherResponseAxios();
+  const WeatherData = await fetchWeatherResponse(city);
 
   // console.log(WeatherData);
 
-  await client.hSet(place, { data: JSON.stringify(WeatherData) });
-  await client.expire(place, 60);
-  console.log(WeatherData);
-  
+  await client.hSet(city, { data: JSON.stringify(WeatherData) });
+  await client.expire(city, 3600);
+  // console.log(WeatherData);
+
   return WeatherData;
 }
 
-getWeatherRedisDetils();
+// getWeatherRedisDetils();
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 15 minutes
+  limit: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+
+  if (!city) {
+    return res.status(400).json({
+      error: "City query parameter is required",
+    });
+  }
+
+  try {
+    const data = await getWeatherDetils(city);
+
+    res.json({
+      success: true,
+      source: "weatherapi + redis",
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost:${PORT}`);
+});
